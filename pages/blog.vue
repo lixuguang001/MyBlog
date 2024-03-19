5bbd62bc7dc81aea9ebae205b1b215e5523db8c5
lixuguangg <lxgxxzj@qq.com>
2024-03-19 17:36:03 +0800

    first commit

---
+++ pages/blog.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { getIncludedYearPosts } from '@/utils'
+
+const posts = await getIncludedYearPosts('blog')
+// console.log('posts', posts)
+</script>
+
+<template>
+  <sub-nav />
+
+  <post-list :posts="posts" />
+</template>
